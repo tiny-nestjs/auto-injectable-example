4f21cc8046c0092ac0123abf5dd1bd5a721eba6a
Nhahan <kisy324@naver.com>
2023-08-12 18:11:04 +0900

    test: v0.2.1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ComponentScan } from "@tiny-nestjs/auto-injectable";
+import { ComponentScan } from '@tiny-nestjs/auto-injectable';
 
 @ComponentScan()
 @Module({
